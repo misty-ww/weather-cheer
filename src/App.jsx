f5911204d67b268b1df97933070199ce50a3e7a1
misty <leva.lewin2017@yandex.ru>
2026-03-25 20:30:41 +0300

    added controller

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,27 @@ function App() {
   function renderError() {
     return <p>{error}</p>;
   }
+  function renderContent() {
+    return (
+      <div className="weather-card">
+        <h2>{`${weatherData?.location?.name}, ${weatherData?.location?.country}`}</h2>
+        <img
+          src={`http:${weatherData?.current?.condition?.icon}`}
+          alt="icon"
+          className="weather-icon"
+        />
+        <p className="temperature">{weatherData?.current?.temp_c}°C</p>
+        <p className="condition">{weatherData?.current?.condition?.text}</p>
+        <div className="weather-details">
+          <p>Humidity: {weatherData?.current?.humidity}%</p>
+          <p>Wind: {weatherData?.current?.wind_kph} km/h</p>
+        </div>
+      </div>
+    );
+  }
   async function searchWeather() {
+    const controller = new AbortController();
+    const signal = controller.signal;
     setError(null);
     if (!city.trim()) {
       setError(null);
@@ -67,6 +87,7 @@ function App() {
 
       const res = await fetch(
         `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${query}`,
+        { signal },
       );
       // if (!res.ok) {
       //   throw new Error(`${res.status}`);
@@ -86,25 +107,9 @@ function App() {
     } finally {
       setLoading(false);
     }
-  }
-
-  function renderContent() {
-    return (
-      <div className="weather-card">
-        <h2>{`${weatherData?.location?.name}, ${weatherData?.location?.country}`}</h2>
-        <img
-          src={`http:${weatherData?.current?.condition?.icon}`}
-          alt="icon"
-          className="weather-icon"
-        />
-        <p className="temperature">{weatherData?.current?.temp_c}°C</p>
-        <p className="condition">{weatherData?.current?.condition?.text}</p>
-        <div className="weather-details">
-          <p>Humidity: {weatherData?.current?.humidity}%</p>
-          <p>Wind: {weatherData?.current?.wind_kph} km/h</p>
-        </div>
-      </div>
-    );
+    return () => {
+      controller.abort();
+    };
   }
 
   useEffect(() => {
